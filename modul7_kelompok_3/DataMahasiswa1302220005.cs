7e5e9aaa78931c9f8825dd25183414d4b919d137
wildanniam <wildanniam4@gmail.com>
2024-04-01 07:58:32 +0700

    menambahkan class TeamMembers1302220005

--- modul7_kelompok_3/DataMahasiswa1302220005.cs
+++ modul7_kelompok_3/DataMahasiswa1302220005.cs
@@ -36,7 +36,7 @@ public class DataMahasiswa1302220005
         var dataMhs = JsonSerializer.Deserialize<DataMahasiswa1302220005>(json);
         if (dataMhs != null)
         {
-            Console.WriteLine($"Nama          : {dataMhs.mahasiswa.firstName} {dataMhs.mahasiswa.lastName}");
+                Console.WriteLine($"Nama          : {dataMhs.mahasiswa.firstName} {dataMhs.mahasiswa.lastName}");
                 Console.WriteLine($"Jenis kelamin : {dataMhs.mahasiswa.gender}");
                 Console.WriteLine($"Umur          : {dataMhs.mahasiswa.age}");
                 Console.WriteLine($"Alamat        : {dataMhs.alamat.streetAddress}, {dataMhs.alamat.city}, {dataMhs.alamat.state}");
---
+++ modul7_kelompok_3/TeamMembers1302220005cs.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+public class TeamMember1302220005
+{
+    public class Members
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string gender { get; set; }
+        public int age { get; set; }
+        public string nim { get; set; }
+    }
+
+    public Members[] member { get; set; }
+
+    public void ReadJSON()
+    {
+        string json = File.ReadAllText("tp7_2_1302220005.json");
+
+        var members = JsonSerializer.Deserialize<TeamMember1302220005>(json);
+
+        Console.WriteLine("Daftar mata kuliah yang diambil:");
+        int i = 0;
+        foreach (var anggota in member)
+        {
+            i++;
+            Console.WriteLine($"Nama : {i} {anggota.firstName} {anggota.lastName} umur {anggota.age} jenis kelamin {anggota.gender} ");
+            Console.WriteLine($"umur : {anggota.age} jenis kelamin {anggota.gender} ");
+            Console.WriteLine($"jenis kelamin : {anggota.gender} ");
+        }
+    }
+}
