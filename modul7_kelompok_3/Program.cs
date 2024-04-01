2793da09f17f508e0a79cb608340baa0a4dc5fa9
ArgaAAL <argalumunon9@gmail.com>
2024-04-01 07:57:33 +0700

    menambahkan class GlossaryItem1302223038

--- modul7_kelompok_3/Program.cs
+++ modul7_kelompok_3/Program.cs
@@ -4,8 +4,10 @@
     {
         DataMahasiswa1302223038 dataArga = new DataMahasiswa1302223038();
         TeamMembers1302223038 timArga = new TeamMembers1302223038();
+        GlosarryItem1302223038 gloss = new GlosarryItem1302223038();
         dataArga.ReadJSON();
         timArga.ReadJSON();
+        gloss.ReadJSON();
 
     }
 }
