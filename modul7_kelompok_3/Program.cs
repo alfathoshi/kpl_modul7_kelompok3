702ae1925678f0c9122c09856f49ee7bb503eef6
unknown <farisazhar.0241@gmail.com>
2024-04-01 08:14:06 +0700

    Menambahkan class TeamMembers1302223123

--- modul7_kelompok_3/Program.cs
+++ modul7_kelompok_3/Program.cs
@@ -3,7 +3,9 @@
     private static void Main(string[] args)
     {
         DataMahasiswa1302223123 dataMhs = new DataMahasiswa1302223123();
+        TeamMembers1302223123 Anggota = new TeamMembers1302223123();
         dataMhs.ReadJSON();
         Console.WriteLine();
+        Anggota.ReadJSON();
     }
 }
---
+++ modul7_kelompok_3/TeamMembers1302223123.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.Json;
+
+public class TeamMembers1302223123
+{
+    public class Members
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string gender { get; set; }
+        public int age { get; set; }
+        public string nim {  get; set; }
+    }
+
+    public Members[] members { get; set; }
+
+    public void ReadJSON()
+    {
+        string json = File.ReadAllText("jurnal7_2_1302223123.json");
+
+        var TeamMembers = JsonSerializer.Deserialize<TeamMembers1302223123>(json);
+
+        Console.WriteLine("Anggota Team:");
+        int i = 0;
+        foreach (var tm in TeamMembers.members)
+        {
+            i++;
+            Console.WriteLine($"Anggota KE {i}-{tm.firstName}{tm.lastName} - {tm.gender}-{tm.age}-{tm.nim}");
+        }
+    }
+}
